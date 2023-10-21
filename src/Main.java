1a9d424750bf6238d9913849b74cffe912dc4534
Expo <crdailey01@gmail.com>
2023-10-21 16:49:42 -0700

    super close... annotated solution

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,40 @@ public class Main {
 
     }
 }
+// i was super close...
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+// do not assign slow.next as null. need it to point to its next node but neext node will pooint back to null
+
+        ListNode prev = null, curr = slow, tmp;
+        while (curr != null) {
+            tmp = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        ListNode first = head, second = prev;
+        while (second.next != null) {
+            tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+    }// loop terminates where last node from 1st half already points to new tail in second half so don't need to add final
+    // pointer to that node
+}
 
 
 class Solution {
@@ -35,6 +69,7 @@ class Solution {
             prev = head2;
             head2 = after;
         }
+
         ListNode dummy = new ListNode(0), mergeTemp = dummy; // merge
         while(head != null && prev != null){
             mergeTemp.next = head;
